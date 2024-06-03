9d330f8b64d377e6c9c34cdec1ba73cbf54812a0
Eren Koc <crestand@outlook.com>
2024-06-03 12:27:36 +0300

    Initialized project

---
+++ LoodosMovie/LoodosMovie/Screens/SplashScreenVC.swift
@@ -0,0 +1,22 @@
+//
+//  SplashScreenVC.swift
+//  LoodosMovie
+//
+//  Created by Hamza Eren Koc on 3.06.2024.
+//
+
+import UIKit
+
+class SplashScreenVC: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemCyan
+        
+    }
+    
+
+
+}
